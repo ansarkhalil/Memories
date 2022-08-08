0b5db1b1fda0b9eedae7fb031220a84350111696
ansarkhalil <ansarkhalil93@gmail.com>
2022-08-08 15:18:58 +0500

    client setup complete

---
+++ client/src/Posts/Posts.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from "react";
+import Post from "./Post/Post";
+
+const Posts = () =>{
+    return (
+        <Fragment>
+            <Post />
+            <Post />
+            <Post />
+        </Fragment>
+    )
+
+}
+
+export default Posts;
